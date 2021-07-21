2256b7a56464e8b97a601c905c987d86ecad14ce
Neha121995 <jagtapneha1212@gmail.com>
2021-07-21 11:36:29 +0530

    [Neha]Added: Welcome to tic toe problem

---
+++ TicTacToe_Problem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TicTacToe_Problem
+{
+    class Program
+    {
+        static void ticTacToeBoard()
+        {
+            int boardSize = 10;
+            char[] board = new char[boardSize];
+            for (int i=1; i<boardSize; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to tic tac toe problem");
+            ticTacToeBoard();
+        }
+    }
+}
